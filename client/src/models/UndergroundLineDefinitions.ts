a6b41463cfb23654e87fe97d04d28366ef4ad547
vojd <mathias.tervo@gmail.com>
2018-11-09 00:48:53 +0100

    Dictionaries converted to functions

--- client/src/models/UndergroundLineDefinitions.ts
+++ client/src/models/UndergroundLineDefinitions.ts
@@ -7,221 +7,6 @@ const addPropsToNodelist = ( nodeList: MapNode[] ): MapNode[] => {
   });
 };
 
-const enum Branches {
-  RED_LINE_NORTHBOUND,
-  RED_LINE_MORBY_CENTRUM,
-  RED_LINE_ROPSTEN,
-
-  RED_LINE_SOUTHBOUND,
-  RED_LINE_MALARHOJDEN,
-  RED_LINE_NORSBORG,
-  RED_LINE_FRUANGEN,
-}
-
-const redLineFruangenNodes: MapNode[] = [
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Midsommarkransen',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Telefonplan',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Hägerstensåsen',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Västertorp',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Fruängen',
-    owner: CompanyDefinitions.CITYCON,
-    x: 0, y: 0,
-  },
-];
-
-const redLineMalarhojdenNodes: MapNode[] = [
-  {
-    filled: -1,
-    direction: 'w',
-
-    name: 'Aspudden',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1,
-    direction: 'w',
-
-    name: 'Örnsberg',
-    owner: null,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'w',
-
-    name: 'Mälarhöjden',
-    owner: null,
-    x: 0, y: 0,
-    branch: [Branches.RED_LINE_NORSBORG]
-  },
-];
-
-const redLineNorsborgNodes: MapNode[] = [
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Bredäng',
-    owner: CompanyDefinitions.FAST_PARTNER,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Sätra',
-    owner: CompanyDefinitions.SATRA,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Skärholmen',
-    owner: CompanyDefinitions.GROSVENOR,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Vårberg',
-    owner: CompanyDefinitions.AGORA,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Vårby Gård',
-    owner: CompanyDefinitions.BALDER,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Fittja',
-    owner: CompanyDefinitions.FITTJA_CENTRUMFASTIGHETER,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Alby',
-    owner: CompanyDefinitions.BOTKYRKABYGGEN,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Hallunda',
-    owner: CompanyDefinitions.STERNER,
-    x: 0, y: 0,
-  },
-
-  {
-    filled: -1,
-    direction: 'sw',
-
-    name: 'Norsborg',
-    owner: CompanyDefinitions.NORSBORG,
-    x: 0, y: 0,
-  },
-];
-
-// red line - south bound from t-centralen
-const redLineSouthBound: MapNode[] = [
-  {
-    filled: -1,
-    direction: 's',
-
-    name: 'Gamla stan',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: 1.0,
-    direction: 'w',
-
-    name: 'Slussen',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1, // negative means non-circular station
-    direction: 'w',
-    name: 'Mariatorget',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1, // negative means non-circular station
-    direction: 'w',
-    name: 'Zinkensdamm',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1, // negative means non-circular station
-    direction: 'w',
-    name: 'Hornstull',
-    owner: null,
-    x: 0, y: 0,
-  },
-  {
-    filled: -1, // negative means non-circular station
-    direction: 'w',
-    name: 'Liljeholmen',
-    owner: null,
-
-    branch: [Branches.RED_LINE_MALARHOJDEN, Branches.RED_LINE_FRUANGEN],
-    x: 0, y: 0,
-  },
-];
-
 const station = ( name: string,
                   direction: string,
                   filled: number = -1,
@@ -244,6 +29,53 @@ const station = ( name: string,
   return s;
 };
 
+const enum Branches {
+  RED_LINE_NORTHBOUND,
+  RED_LINE_MORBY_CENTRUM,
+  RED_LINE_ROPSTEN,
+
+  RED_LINE_SOUTHBOUND,
+  RED_LINE_MALARHOJDEN,
+  RED_LINE_NORSBORG,
+  RED_LINE_FRUANGEN,
+}
+
+const redLineFruangenNodes: MapNode[] = [
+  station('Midsommarkransen', 'sw'),
+  station('Telefonplan', 'sw'),
+  station('Hägerstensåsen', 'sw'),
+  station('Västertorp', 'sw'),
+  station('Fruängen', 'sw'),
+];
+
+const redLineMalarhojdenNodes: MapNode[] = [
+  station('Aspudden', 'w'),
+  station('Örnsberg', 'w'),
+  station('Mälarhöjden', 'w', -1, 1, null, [Branches.RED_LINE_NORSBORG]),
+];
+
+const redLineNorsborgNodes: MapNode[] = [
+  station('Bredäng', 'sw', -1, 1, CompanyDefinitions.FAST_PARTNER),
+  station('Sätra', 'sw', -1, 1, CompanyDefinitions.SATRA),
+  station('Skärholmen', 'sw', -1, 1, CompanyDefinitions.GROSVENOR),
+  station('Vårberg', 'sw', -1, 1, CompanyDefinitions.AGORA),
+  station('Vårby Gård', 'sw', -1, 1, CompanyDefinitions.BALDER),
+  station('Fittja', 'sw', -1, 1, CompanyDefinitions.FITTJA_CENTRUMFASTIGHETER),
+  station('Alby', 'sw', -1, 1, CompanyDefinitions.BOTKYRKABYGGEN),
+  station('Hallunda', 'sw', -1, 1, CompanyDefinitions.STERNER),
+  station('Norsborg', 'sw', -1, 1, CompanyDefinitions.NORSBORG),
+];
+
+// red line - south bound from t-centralen
+const redLineSouthBound: MapNode[] = [
+  station('Gamla stan', 's'),
+  station('Slussen', 's'),
+  station('Mariatorget', 'w'),
+  station('Zinkensdamm', 'w'),
+  station('Hornstull', 'w'),
+  station('Liljeholmen', 'w', -1, 1, null, [Branches.RED_LINE_MALARHOJDEN, Branches.RED_LINE_FRUANGEN]),
+];
+
 // north bound from t-centralen
 const redLineNorthBound: MapNode[] = [
   station('Östermalms torg', 'n', -1, 1, null, [
@@ -253,12 +85,12 @@ const redLineNorthBound: MapNode[] = [
 ];
 
 const redLineMorbyCentrum: MapNode[] = [
-  station( 'Stadion', 'n', -1, 3),
-  station( 'Tekniska högskolan', 'n', ),
-  station( 'Universitetet', 'n', ),
-  station( 'Bergshamra', 'n', ),
-  station( 'Danderyds sjukhus', 'n', ),
-  station( 'Mörby Centrum', 'n', ),
+  station('Stadion', 'n', -1, 3),
+  station('Tekniska högskolan', 'n'),
+  station('Universitetet', 'n'),
+  station('Bergshamra', 'n'),
+  station('Danderyds sjukhus', 'n'),
+  station('Mörby Centrum', 'n'),
 ];
 
 const redLineRopsten: MapNode[] = [
