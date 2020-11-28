a906afc21fe629e4714719871a9502a50bccdc5a
Mathias Tervo <mathias.tervo@gmail.com>
2020-11-28 20:50:12 +0100

    changed about map text

--- client/src/components/SidebarAbout.tsx
+++ client/src/components/SidebarAbout.tsx
@@ -8,47 +8,30 @@ const AboutMap = () => {
       <h4>About the map</h4>
 
       <p>
-        “Nesting today in trade names are figments such as those earlier thought to be hidden in the cache of "poetic"
-        vocables.”
-        <br/>
-        Walter Benjamin, The Arcade Project
-      </p>
-
-      <p>
-        ”The map will hinder the mapping, as we come to be captivated by fetishes of scale and precision that smooth
-        over the world's contradictions.” <br/>
-        Alberto Toscano and Jeff Kinkle, Cartographies of the Absolute
-      </p>
-
-      <p>
-        The map was drafted with the aim to collect and share information about the property owners of local centers in
-        Stockholm. This mapping gives access to knowledge and makes the ownership structures in the city more visible.
+        This map of the Stockholm metro system was drafted with the aim to collect and share information about the
+        property owners of local centers, in order to make ownership structures in the city more visible.
         These structures gravely affect the places we abide, consequently shaping our daily lives.
-        The map encourages one to turn the focus towards the owning class, and in that sense allows for more groups and
-        individuals to participate in an exploration of the city's invisible layers.
-      </p>
-
-      <p className="bordered-text">
-        The categories “privately owned” and “publicly owned” refer to the ownership status of the built environment
-        around each center.
-        The year of sale refers to when the property shifted from public to private ownership.
-        Our sources of information include municipalities,
-        The National Land Survey as well as local and financial press.
+        The map encourages one to turn the focus towards the owning class and financial elites,
+        and in that sense allows for more groups and individuals to participate in an exploration of the
+        city’s fundamental but often obscured layers.
       </p>
 
       <p>
-        The map will be updated during the exhibition with additional categories and materials.
+        The filter categories “privately owned” and “publicly owned” refer to the ownership status of the
+        built environment around each center. The year of sale refers to when the property shifted from public to
+        private ownership. Sources of information include municipalities, The National Land Survey as well as the
+        local and financial press. Additional categories and materials will be added gradually.
         Feel free to contact us if you have any questions or want to discuss the design or content!
       </p>
 
       <p>
-        The map is produced by Mapping the Unjust City in collaboration with
-        Mathias Tervo, Channa Bianca and Scott Springfeldt. Film by: Samuel Richter.
+        The map is produced by Mapping the Unjust City in collaboration with Mathias Tervo, Channa Bianca,
+        and Scott Springfeldt. Intro video by Samuel Richter.
       </p>
 
       <p>
-        The map has been produced with support from, among others, Kulturbryggan, Marabouparken konsthall,
-        Iaspis Konstnärsnämnden and RISD´s Architecture.
+        With support from, among others, Kulturbryggan, Marabouparken konsthall,
+        Iaspis (Swedish Arts Grants Committee) and RISD Architecture.
       </p>
 
       <p>A version in Swedish is planned to be launched in the winter of 2021.</p>
@@ -62,13 +45,9 @@ const AboutMap = () => {
       </p>
 
       <p>
-        The group was formed in 2015 in Stockholm with members working in the fields of art, design, architecture and
-        aesthetics. Since then the group has worked with mappings and cartography in the form of sound pieces,
-        video essays, photography, writings and maps.
-      </p>
-
-      <p>
-        Group members include Maryam Fanni, Elof Hellström, Åsa Johansson, Sarah Kim, Paula Urbano.
+        The group was formed in 2015 in Stockholm with members working in the fields of art, design, architecture,
+        and aesthetics. Since then the group has worked in a tradition of critical cartography and counter-mapping
+        producing sound pieces, video essays, photography, writings, and maps.
       </p>
     </div>
   );
